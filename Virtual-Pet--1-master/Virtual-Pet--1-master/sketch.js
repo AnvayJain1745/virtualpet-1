a49a02a3b95f20493d0c4bfed0441ec088717be2
AnvayJain1745 <68576271+AnvayJain1745@users.noreply.github.com>
2020-10-21 17:05:21 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,28 @@
+//Create variables here
+var dog,happyDog;
+
+var database;
+
+var foodS,foodStock;
+function preload()
+{
+  //load images here
+  dog=loadImage("Virtual-Pet--1-master/images/doglmg.png")
+  
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
